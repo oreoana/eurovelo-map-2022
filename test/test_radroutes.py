c4d48b768e6b32dddf8d0a72e529e372b90ce480
Eric <ricehornet@gmail.com>
2023-02-20 21:37:48 +0000

    fix basic test

--- test/test_radroutes.py
+++ test/test_radroutes.py
@@ -1,8 +1,11 @@
-import pytest
-from src.radroutes import RadRouter
+import folium
+from radroutes import RadRouter
 
 class TestRadRouter(object):
-    def test__radrouter__process__process_path__ok(self):
-        rr = RadRouter()
-        path = "/tmp/test.gpx"
-        assert path == rr.process(path)
+    def test__radrouter__init__ok(self):
+        path_to_config = "/path/to/config"
+        path_to_output = "/path/to/output"
+        rr = RadRouter(path_to_config, path_to_output)
+        assert rr.config_file_path == path_to_config
+        assert rr.out_file_path == path_to_output
+        assert isinstance(rr.map, folium.Map)
